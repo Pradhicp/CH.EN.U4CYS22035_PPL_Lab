5b0bfb3313f4f895adf9cebb5c2a15d9e7a557d5
Pradheeba P <147037709+Pradhicp@users.noreply.github.com>
2025-02-21 14:29:18 +0530

    Create datatype.rs

---
+++ Lab_7/datatype.rs
@@ -0,0 +1,19 @@
+fn main() { 
+
+    let integer: i32 = 42; 
+
+    let float: f64 = 3.14; 
+
+    let boolean: bool = true; 
+
+    let character: char = 'A'; 
+
+    println!("Integer: {}", integer); 
+
+    println!("Float: {}", float); 
+
+    println!("Boolean: {}", boolean); 
+
+    println!("Character: {}", character); 
+
+} 
